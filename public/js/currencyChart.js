d9205926c55d3bac630f57a4d34a9708adf7eb3d
George Paresishvili <g.paresishvili@outlook.com>
2022-03-08 17:03:14 +0400

    chart is now managed in this file

--- public/js/currencyChart.js
+++ public/js/currencyChart.js
@@ -1,24 +1,29 @@
-fetch('https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/en/json')
-.then(response => response.json())
-.then(obj => obj[0].date);
+let statistics = document.getElementById('statistics');
+let dates = JSON.parse(statistics.dataset.dates);
+let rates = JSON.parse(statistics.dataset.rates);
 
 let data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  labels: dates,
   datasets: [
     {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: ['ea3724'],
-      borderColor: ['ea3724'],
+      label: "# Rate",
+      data: rates,
+      backgroundColor: ["ea3724"],
+      borderColor: ["ea3724"],
       borderWidth: 1,
     },
   ],
-}
+};
 
 let options = {
   scales: {
     y: {
       beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
     },
   },
 };
@@ -27,5 +32,9 @@ const ctx = document.getElementById("currHistCanv").getContext("2d");
 const myChart = new Chart(ctx, {
   type: "line",
   data: data,
-  options: options
+  options: options,
 });
+
+fetch("https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/en/json")
+  .then((response) => response.json())
+  .then((obj) => obj[0].date);
