247ad9f9463650935f3e1074a2d61908c521ee3b
George Paresishvili <g.paresishvili@outlook.com>
2022-03-08 16:56:44 +0400

    moved cronjob to updateCurrencyDatabase.js

--- controllers/forex.js
+++ controllers/forex.js
@@ -1,13 +1,3 @@
-import fetch from "node-fetch";
-import Forex from "../models/Forex.js";
-
 export default async (req, res) => {
-  let nbgResponse = await fetch("https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/en/json");
-  let data = await nbgResponse.json();
-  let date = data[0].date;
-
-  Forex.findOne({ date }, (err, obj) => {
-    if (!obj) Forex.create(data[0]);
-  });
   res.render("forex");
 };
