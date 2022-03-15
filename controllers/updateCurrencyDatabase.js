b58db9714f1c7ddb5bae5df2b743ed2c8eef6b21
George Paresishvili <g.paresishvili@outlook.com>
2022-03-15 11:49:48 +0400

    cron job set to every 6th hour; added error handling

--- controllers/updateCurrencyDatabase.js
+++ controllers/updateCurrencyDatabase.js
@@ -9,28 +9,32 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 function updateCurrencyDatabase() {
-  schedule.scheduleJob("0 */9 * * *", async function () {
-    let nbgResponse = await fetch("https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/en/json");
-    let data = await nbgResponse.json();
-    let date = data[0].date;
-
-    let obj = await Forex.findOne({ date });
-    // If object does not exist then create a document
-    if (!obj) {
-      await Forex.create(data[0]);
-    }
-
-    const logDir = path.join(__dirname, "logs");
-
+  const job = schedule.scheduleJob("0 0 */6 * * *", async function () {
     try {
-      await fs.promises.mkdir(logDir);
-    } catch (e) {
-      console.log(`called ${e.syscall}; path ${e.path} already exists`);
+      let nbgResponse = await fetch("https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/en/json");
+      let data = await nbgResponse.json();
+      let date = data[0].date;
+
+      let obj = await Forex.findOne({ date });
+      // If object does not exist then create a document
+      if (!obj) {
+        await Forex.create(data[0]);
+      }
+
+      const logDir = path.join(__dirname, "logs");
+
+      try {
+        await fs.promises.mkdir(logDir);
+      } catch (e) {
+        console.log(`called ${e.syscall}; path ${e.path} already exists`);
+      }
+
+      const reportPath = path.join(logDir, "currencyReport.txt");
+
+      await fs.promises.appendFile(reportPath, `${new Date(Date.now()).toUTCString()}: Success. Data is now for ${new Date(date).toUTCString()} \n`);
+    } catch (err) {
+      console.log(err);
     }
-
-    const reportPath = path.join(logDir, "currencyReport.txt");
-
-    await fs.promises.appendFile(reportPath, `${new Date(Date.now()).toUTCString()}: Success. Data is now for ${new Date(date).toUTCString()} \n`);
   });
 }
 
